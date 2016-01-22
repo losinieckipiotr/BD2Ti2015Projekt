acea46ae2d4ae8a3530018a7056de8ed36d1504a
losinieckipiotr <losinieckipiotr@gmail.com>
2016-01-22 22:29:09 +0100

    zmiany

--- Stocktaking/View/RoomsView.xaml.cs
+++ Stocktaking/View/RoomsView.xaml.cs
@@ -74,9 +74,6 @@ namespace Stocktaking.View
                     rekordy.Add(new RoomRecord(s));
                 }
                 roomRecordViewSource.Source = rekordy.OrderBy(r => r.id);
-                //System.Windows.Data.CollectionViewSource salaViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["salaViewSource"];
-                //db.sala.Load();
-                //salaViewSource.Source = db.sala.Local.ToBindingList();
 
                 System.Windows.Data.CollectionViewSource sala_typViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["sala_typViewSource"];
                 db.sala_typ.Load();
@@ -117,7 +114,6 @@ namespace Stocktaking.View
             if (!char.IsDigit(e.Text, e.Text.Length - 1))
             {
                 e.Handled = true;
-
             }
         }
 
@@ -125,16 +121,22 @@ namespace Stocktaking.View
         {
             try
             {
-                if (!Potwierdz("Czy chcesz zaktualizować dane sali?"))
+                if (!ViewLogic.Potwierdz("Czy chcesz zaktualizować dane sali?"))
                     return;
 
                 RoomRecord r = (RoomRecord)salaDataGrid.SelectedItem;
                 sala_typ st = (sala_typ)RoomType.SelectedItem;
-                //zaklad z = (zaklad)RoomInstitute.SelectedItem;
-                r.s.numer = Convert.ToInt32(RoomNumber.Text);
+                int nowyNumer = Convert.ToInt32(RoomNumber.Text);
+                bool numerZajety = db.sala.Any(s => s.numer == nowyNumer);
+                if (numerZajety)
+                {
+                    ViewLogic.Blad("Isnieje już sala o podanym numerze!");
+                    return;
+                }
+                r.s.numer = nowyNumer;
                 r.s.pojemnosc = Convert.ToInt32(RoomCapacity.Text);
                 r.s.sala_typ = st;
-                //s.zaklad = z;
+
                 db.SaveChanges();
 
                 OdswiezSale();
@@ -145,60 +147,23 @@ namespace Stocktaking.View
             }
         }
 
-        private void OdswiezSale()
-        {
-            System.Windows.Data.CollectionViewSource roomRecordViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["roomRecordViewSource"];
-            //db.sala.Load();
-            roomRecordViewSource.Source = null;
-            List<sala> sale = db.sala.Local.ToList();
-            List<RoomRecord> rekordy = new List<RoomRecord>();
-            foreach (sala s in sale)
-            {
-                rekordy.Add(new RoomRecord(s));
-            }
-            roomRecordViewSource.Source = rekordy.OrderBy(r => r.id);
-        }
-
-        private bool Potwierdz(string pytanie)
-        {
-            MessageBoxResult result = MessageBox.Show
-                   (pytanie,
-                   "Pytanie",
-                   MessageBoxButton.YesNo,
-                   MessageBoxImage.Question,
-                   MessageBoxResult.No);
-            if (result == MessageBoxResult.Yes)
-                return true;
-            else
-                return false;
-        }
-
-        private void Blad(string wiadomosc)
-        {
-            MessageBox.Show(
-                       wiadomosc,
-                       "Błąd",
-                       MessageBoxButton.OK,
-                       MessageBoxImage.Error);
-        }
-
         private void RoomAdd_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                if (!Potwierdz("Czy chcesz dodać salę?"))
+                if (!ViewLogic.Potwierdz("Czy chcesz dodać salę?"))
                     return;
 
                 string nowyNumerS = RoomNumber.Text;
                 string nowaPojemnoscS = RoomCapacity.Text;
                 if (nowyNumerS == "")
                 {
-                    Blad("Nie podano numeru!");
+                    ViewLogic.Blad("Nie podano numeru!");
                     return;
                 }
                 if (nowaPojemnoscS == "")
                 {
-                    Blad("Nie podano pojemności!");
+                    ViewLogic.Blad("Nie podano pojemności!");
                     return;
                 }
                 int nowyNumer = Convert.ToInt32(nowyNumerS);
@@ -207,7 +172,7 @@ namespace Stocktaking.View
                 bool numerZajety = db.sala.Any(s => s.numer == nowyNumer);
                 if (numerZajety)
                 {
-                    Blad("Isnieje już sala o podanym numerze!");
+                    ViewLogic.Blad("Isnieje już sala o podanym numerze!");
                     return;
                 }
 
@@ -221,7 +186,6 @@ namespace Stocktaking.View
                 }
 
                 sala_typ nowyTyp = (sala_typ)RoomType.SelectedItem;
-                //zaklad nowyZaklad = (zaklad)RoomInstitute.SelectedItem;
 
                 sala nowy = new sala
                 {
@@ -230,11 +194,10 @@ namespace Stocktaking.View
                     pojemnosc = nowaPojemnosc,
                     sala_typ = nowyTyp,
                     sala_typ_id = nowyTyp.id,
-                    //zaklad = nowyZaklad,
-                    //zaklad_id = nowyZaklad.id
                 };
                 db.sala.Add(nowy);
                 db.SaveChanges();
+
                 OdswiezSale();
             }
             catch (Exception)
@@ -247,20 +210,34 @@ namespace Stocktaking.View
         {
             try
             {
-                if (!Potwierdz("Czy chcesz usunąć salę?"))
+                if (!ViewLogic.Potwierdz("Czy chcesz usunąć salę?"))
                     return;
 
                 RoomRecord r = (RoomRecord)salaDataGrid.SelectedItem;
+
                 db.sala.Remove(r.s);
                 db.SaveChanges();
-                OdswiezSale();
 
+                OdswiezSale();
             }
             catch (Exception)
             {
 
                 throw;
             }
+        }
+
+        private void OdswiezSale()
+        {
+            System.Windows.Data.CollectionViewSource roomRecordViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["roomRecordViewSource"];
+            db.sala.Load();
+            List<sala> sale = db.sala.Local.ToList();
+            List<RoomRecord> rekordy = new List<RoomRecord>();
+            foreach (sala s in sale)
+            {
+                rekordy.Add(new RoomRecord(s));
+            }
+            roomRecordViewSource.Source = rekordy.OrderBy(r => r.id);
         }
     }
 }
